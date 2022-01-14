8e737fcb9e762500a9b9bebb5de25255193f449c
BetsyArinze <88517681+BetsyArinze@users.noreply.github.com>
2022-01-14 13:24:52 +0100

    SQLite Database
    Got the app to read data from the  SQLite database

--- app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -2,9 +2,15 @@ package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.database.Cursor;
+
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -16,19 +22,43 @@ public class DrinkActivity extends AppCompatActivity {
 
         //Get the drink from the intent
         int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
-        Drink drink = Drink.drinks[drinkId];
 
-        //Populate the drink name
-        TextView name = (TextView)findViewById(R.id.name);
-        name.setText(drink.getName());
+        //create a cursor
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try{
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query ("DRINK",
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                            "_id = ?",
+                            new String[] {Integer.toString(drinkId)},
+                            null, null, null);
 
-        //Populate the drink description
-        TextView description = (TextView)findViewById(R.id.description);
-        description.setText(drink.getDescription());
+            //Move the first record in the cursor
+            if (cursor.moveToFirst()){
+                //Get the drink details from the cursor
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
 
-        //Populate the drink image
-        ImageView photo = (ImageView)findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getName());
+                //Populate the drink name
+                TextView name = (TextView)findViewById(R.id.name);
+                name.setText(nameText);
+
+                //Populate the drink description
+                TextView description = (TextView)findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                //Populate the drink image
+                ImageView photo = (ImageView)findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+            cursor.close();
+            db.close();
+            } catch (SQLiteException e){
+                Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
     }
 }
--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -8,28 +8,49 @@ import android.widget.ListView;
 import android.view.View;
 import android.content.Intent;
 import android.widget.AdapterView;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks
-        );
-        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
 
-        //Create the listener
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try{
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        }catch (SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        //Create the listener to listen for clicks in the list view
         AdapterView.OnItemClickListener itemClickListener =
-                new AdapterView.OnItemClickListener(){
+                new AdapterView.OnItemClickListener() {
                     public void onItemClick(AdapterView<?> listDrinks,
-                                            View itemView,
+                                            View itemview,
                                             int position,
-                                            long id){
+                                            long id) {
                         //Pass the drink the user clicks on to DrinkActivity
                         Intent intent = new Intent(DrinkCategoryActivity.this,
                                 DrinkActivity.class);
@@ -39,5 +60,12 @@ public class DrinkCategoryActivity extends AppCompatActivity {
                 };
         //Assign the listener to the list view
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
